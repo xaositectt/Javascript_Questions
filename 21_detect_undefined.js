97892a88ad987343dc0c1163123907e9cd36b30c
xaositectt <tamas.varga.420@gmail.com>
2018-05-21 00:16:56 +0200

    added examples 21 and 22

---
+++ 21_detect_undefined.js
@@ -0,0 +1,6 @@
+'use strict'
+
+//detect if an object property is undefined
+if (typeof my_obj.someproperties === "undefined"){
+  console.log('the property is not available...'); 
+}
---
+++ 22_key_exists.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// test the particular object instance for a property:
+
+obj.hasOwnProperty("key")
+
+// note the three equal signs so that null won't be equal to undefined
+if( aa["goodbye"] === undefined ) {
+    // do something
+}
