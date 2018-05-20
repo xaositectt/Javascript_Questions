5cba3a328d997f4f3166e6a115e3f31020baa682
xaositectt <tamas.varga.420@gmail.com>
2018-05-20 23:28:30 +0200

    added examples 18 and 19

---
+++ 18_singleton.js
@@ -0,0 +1,28 @@
+'use strict'
+
+//design pattern that restricts the instantiation of a class to one object. A singleton should be immutable by the consuming code.
+//with Object.freeze, we make the instance of the class immutable.
+
+class UserStore {
+  constructor(){
+   if(! UserStore.instance){
+     this._data = [];
+     UserStore.instance = this;
+   }
+
+   return UserStore.instance;
+  }
+
+ add(item){
+    this._data.push(item);
+  }
+
+  get(id){
+    return this._data.find(d => d.id === id);
+  }
+}
+
+const instance = new UserStore();
+Object.freeze(instance);
+
+export default instance;
---
+++ 19_object_creation.js
@@ -0,0 +1,31 @@
+'use strict'
+//different ways to create an object:
+
+//Creates empty object:
+var d = new Object();
+
+//creates a new object extending the prototype object passed as a parameter.
+var a = Object.create(null);
+
+//Equivalent to Object.create(null) method, using a null prototype as an argument.
+var b = {};
+
+// call a function and settisng this of the function to a fresh new Object, and binding 
+// the prototype of that new Object to the function's prototype. 
+var Obj = function(name) {
+  this.name = name
+}
+var c = new Obj("hello"); 
+
+// Using the function constructor + prototype:
+function myObj(){};
+myObj.prototype.name = "hello";
+var k = new myObj();
+
+// Using ES6 class syntax:
+class myObject  {
+  constructor(name) {
+    this.name = name;
+  }
+}
+var e = new myObject("hello");
