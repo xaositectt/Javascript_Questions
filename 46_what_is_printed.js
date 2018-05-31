af60266021deaec295fc122427949bee33f28c27
xaositectt <tamas.varga.420@gmail.com>
2018-05-31 16:44:46 +0200

    added examples 44-46

---
+++ 46_what_is_printed.js
@@ -0,0 +1,5 @@
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 == 0.3);
+
+// Numbers in JavaScript are all treated with floating point precision, 
+// and as such, may not always yield the expected results.
