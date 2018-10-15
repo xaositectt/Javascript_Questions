4f21e152cb858470c20a48e1950f83dec7657b68
tamas.varga.83 <tamas.varga.420@gmail.com>
2018-10-15 15:20:02 +0200

    extended exercise 16

--- 16_functions_methods_constructors.js
+++ 16_functions_methods_constructors.js
@@ -12,7 +12,7 @@ var obj = {
   name: 'John Carter',
   helloWorld: function () {
     console.log(`hello world, ${this.name}`);
-  },
+  }
 }
 
 obj.helloWorld(); // "hello world John Carter"
@@ -20,10 +20,25 @@ obj.helloWorld(); // "hello world John Carter"
 //we can copy a reference to the same function helloWorld in another object and get a different answer.
 var obj2 = {
   name: 'Paige',
-  helloWorld: obj.helloWorld,
+  helloWorld: obj.helloWorld
 }
 
 obj2.helloWorld();
+
+
+//undefined because no name property here
+var obj3 = {
+  helloWorld: obj.helloWorld
+}
+
+// undefined this way
+obj3.helloWorld();
+
+// it's now bound to be Paige
+var obj4 = {
+  helloWorld: obj.helloWorld.bind(obj2)
+}
+obj4.helloWorld();
 
 console.log('when called in a setTimeout the method loses its "this" reference')
 setTimeout(obj.helloWorld, 0)
