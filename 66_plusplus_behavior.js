18f257cb5444d475fc1feeb6f81fcef8715c135a
tamas.varga.83 <tamas.varga.420@gmail.com>
2018-10-16 13:36:46 +0200

    adjusted examples 61 and 66

--- 66_plusplus_behavior.js
+++ 66_plusplus_behavior.js
@@ -3,11 +3,13 @@
 //if you use the ++ expression like this, it actually increments and then assigns.
 //it goes in order! so first assignment, then ++ ++ then assignment again.
 //++a++ throws a referenceerror.
-//then the 
+//then the
 var a = 42;
 var b = a++;
-var c = ++a;
 
-console.log(a);	// 44
+console.log(a);	// 43
 console.log(b);	// 42
+console.log(a);	// 43
+
+var c = ++a;
 console.log(c);	// 44
