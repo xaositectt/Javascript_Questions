287dde13b47247fe58bd8b19bd857dce4a1fe558
tamas.varga.83 <tamas.varga.420@gmail.com>
2018-10-16 11:34:15 +0200

    extended examples 42 and 48

--- 42_array_destructuring.js
+++ 42_array_destructuring.js
@@ -7,5 +7,11 @@ let a, b, c, rest;
 
 console.log(a, b, c);
 
+// doesn't work like this, you need to declare the variables
+// [d, e, f] = [1, 2, 3];
+
+let [d, e, f] = ['d', 'e', 'f'];
+console.log(d, e, f);
+
 [a, b, c, ...rest] = [1, 2, 3, 4, 5, 6, 7];
 console.log(rest);
--- 48_binding_revisited.js
+++ 48_binding_revisited.js
@@ -1,4 +1,4 @@
-//defaut behavior where this defaults to the global object is not available in strict mode so strict mode is not used here
+//default behavior where this defaults to the global object is not available in strict mode so strict mode is not used here
 // 'use strict'
 
 //normal functions lose the implicitly added this if they're assigned as an object's method if you call them in a different context
@@ -16,26 +16,27 @@ let thisIsCrazy = {
 
 thisIsCrazy.func();
 
-setTimeout(thisIsCrazy.func, 1000);
+setTimeout(thisIsCrazy.func, 0);
 
 
 
-//but arrow functions inherit the this from the parent
-//so they don't lose the this if you call them from a different context
+//  but arrow functions inherit the this from the parent
+//  so they don't lose the this if you call them from a different context
+// these don't work!!!
 function callMeMaybe() {
   return () => { console.log('arrow function: ', this.a)}
 }
 
-let wereYouThinkWereGoingBaby = { a:1, func: callMeMaybe };
+let wereYouThinkWereGoingBaby = { a:1, func: callMeMaybe() };
 
-wereYouThinkWereGoingBaby.func()();
+wereYouThinkWereGoingBaby.func();
 
-setTimeout(wereYouThinkWereGoingBaby.func(), 2000);
+setTimeout(wereYouThinkWereGoingBaby.func, 2000);
 
 //this is equal to this, where you pass this with a lexical scoping rule
 //thus escaping using binding or the rules of this
-let rippedJeans =  { 
-  a: 'skin was showin', 
+let rippedJeans =  {
+  a: 'skin was showin',
   func: function yourStareWasHolding() {
     let self = this;
     return function(){ console.log('escaping this rules with this assignment: ', self.a) }
