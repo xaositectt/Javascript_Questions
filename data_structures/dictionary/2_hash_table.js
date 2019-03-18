1ba690de0171fdaf99bab212a3c8d714820b89e8
xaositectt <xaositectt@gmail.com>
2019-03-18 14:39:54 +0100

    removed semicolons

--- data_structures/dictionary/2_hash_table.js
+++ data_structures/dictionary/2_hash_table.js
@@ -1,100 +1,100 @@
 
-const defaultToString = require('./utils.js').defaultToString;
-const ValuePair = require('./utils.js').ValuePair;
+const defaultToString = require('./utils.js').defaultToString
+const ValuePair = require('./utils.js').ValuePair
 
 
 class HashTable {
   constructor(toStrFn = defaultToString) {
-    this.toStrFn = toStrFn;
-    this.table = {};
+    this.toStrFn = toStrFn
+    this.table = {}
   }
 
-  loseloseHashCode(key) {
+  generateHashCode(key) {
     if (typeof key === 'number') {
-      return key;
+      return key
     }
-    const tableKey = this.toStrFn(key);
-    let hash = 0;
-    for (let i = 0; i < tableKey.length; i++) {
-      hash += tableKey.charCodeAt(i);
+    const tableKey = this.toStrFn(key)
+    let hash = 0
+    for (let i = 0 i < tableKey.length i++) {
+      hash += tableKey.charCodeAt(i)
     }
-    // lower the number by dividint with an arbitrary number
-    return hash % 37;
+    // lower the number by dividing with an arbitrary number
+    return hash % 37
   }
 
   hashCode(key) {
-    return this.loseloseHashCode(key);
+    return this.generateHashCode(key)
   }
 
   put(key, value) {
     if (key != null && value != null) {
-      const position = this.hashCode(key);
-      this.table[position] = new ValuePair(key, value);
-      return true;
+      const position = this.hashCode(key)
+      this.table[position] = new ValuePair(key, value)
+      return true
     }
-    return false;
+    return false
   }
 
   get(key) {
-    const valuePair = this.table[this.hashCode(key)];
-    return valuePair == null ? undefined : valuePair.value;
+    const valuePair = this.table[this.hashCode(key)]
+    return valuePair == null ? undefined : valuePair.value
   }
 
   remove(key) {
-    const hash = this.hashCode(key);
-    const valuePair = this.table[hash];
+    const hash = this.hashCode(key)
+    const valuePair = this.table[hash]
     if (valuePair != null) {
-      delete this.table[hash];
-      return true;
+      delete this.table[hash]
+      return true
     }
-    return false;
+    return false
   }
 
   keyValues() {
-    return Object.values(this.table);
+    return Object.values(this.table)
   }
 
   values() {
-    return this.keyValues().map(valuePair => valuePair.value);
+    return this.keyValues().map(valuePair => valuePair.value)
   }
 
   size() {
-    return this.keyValues().length;
+    return this.keyValues().length
   }
 
   isEmpty() {
-    return this.size() === 0;
+    return this.size() === 0
   }
 
   clear() {
-    this.table = {};
+    this.table = {}
   }
 
   toString() {
     if (this.isEmpty()) {
-      return '';
+      return ''
     }
-    const keys = Object.keys(this.table);
-    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
-    for (let i = 1; i < keys.length; i++) {
-      objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`;
+    const keys = Object.keys(this.table)
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`
+    for (let i = 1 i < keys.length i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`
     }
-    return objString;
+    return objString
   }
 }
 
-const hash = new HashTable();
-hash.put('Gandalf', 'gandalf@email.com');
-hash.put('John', 'johnsnow@email.com');
-hash.put('Tyrion', 'tyrion@email.com');
+const hash = new HashTable()
+hash.put('Gandalf', 'gandalf@email.com')
+hash.put('John', 'johnsnow@email.com')
+hash.put('Tyrion', 'tyrion@email.com')
 
-console.log(hash.hashCode('Gandalf') + ' - Gandalf');
-console.log(hash.hashCode('John') + ' - John');
-console.log(hash.hashCode('Tyrion') + ' - Tyrion');
-console.log(hash.keyValues());
-console.log(hash.size());
-console.log(hash.toString());
-hash.clear();
-console.log(hash.size());
-console.log(hash.isEmpty());
-console.log(hash.toString());
+console.log(hash.hashCode('Gandalf') + ' - Gandalf')
+console.log(hash.hashCode('John') + ' - John')
+console.log(hash.hashCode('Tyrion') + ' - Tyrion')
+console.log(hash.keyValues())
+console.log(hash.size())
+console.log(hash.toString())
+hash.clear()
+console.log(hash.size())
+console.log(hash.isEmpty())
+console.log(hash.toString())
